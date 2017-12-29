15e03a7d2ca709ca4ec9936da8ada49491a2a48f
Mohamed Aly <alydemah@gmail.com>
2017-12-29 17:52:41 +0100

    connecting redux to react app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Scoreboard from './containers/Scoreboard';
+
+render(
+    <Scoreboard />,
+    document.getElementById('root')
+
+);
